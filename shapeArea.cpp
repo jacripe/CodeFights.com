5fa3b68548533f94982849ccf018736ce7bdd659
Joshua Cripe <admin@wiseeyesent.com>
2017-05-07 22:35:35 -0500

    Added notes to shapeArea.cpp

--- shapeArea.cpp
+++ shapeArea.cpp
@@ -4,7 +4,10 @@ int shapeArea(int n) {
     return (n == 1) ? n : (shapeArea(n-1)+(4*(n-1)));
 }
 
-// 1:1, 2:5, 3:13, 4:25
+// CHALLENGE: Calculate area for an expanding polygon fitting pattern:
+// n=1:1, n=2:5, n=3:13, n=4:25
+//
+// NOTES:
 //   =   +4    +8   +12
 //   =   +4   +12   +24
 //   n
